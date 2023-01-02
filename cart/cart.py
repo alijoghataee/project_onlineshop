f040f56c8c99c791bde49484f358885569122a28
alijoghatee <117152318+alijoghatee@users.noreply.github.com>
2023-01-02 10:42:19 +0330

    add other method

--- cart/cart.py
+++ cart/cart.py
@@ -1,3 +1,4 @@
+from products.models import Product
 
 
 class Cart:
@@ -43,3 +44,30 @@ class Cart:
         """save session change"""
         self.session.modified = True
 
+    def __iter__(self):
+        product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=product_ids)
+
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product_obj'] = product
+
+        for item in cart.values():
+            yield item
+
+    def __len__(self):
+        return len(self.cart.keys())
+
+    def clear(self):
+        del self.session['cart']
+
+        self.save()
+
+    def get_total_price(self):
+        product_ids = self.cart.keys()
+
+        products = Product.objects.filter(id__in=product_ids)
+
+        return sum(product.price for product in products)
